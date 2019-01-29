44b62fe2a7bc342b19f8d634670f1ab2c6c728da
Gyfu <hthor35@gmail.com>
2019-01-29 14:58:07 +0000

    verkefni3 tilbúið(fyrirutan myndir)

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ frettir=[
         ["Banaslys á sjó","Lorem Ipsum","htj@htj.is"],
         ["Fiskveiðifrétt","Lorem Ipsum","bb@bb.is"],
         ["Stríð í Gaza","Lorem Ipsum","frs@frs.com"],
-        ["Kína er að gera eitthvað, ég veit ekki","Lorem Ipsum","cnn@cnn.is"]
+        ["Kína er að gera eitthvað, ég veit ekki","Lorem Ipsum","cnn@cnn.is"],
         ]
 @app.route('/')
 def main():
-    return render_template("base.html",title="Verkefni3 - Fréttir")
+    return render_template("base.html",title="Verkefni3 - Fréttir", titill="Test")
 @app.route('/frett/<int:num>')
-def frettir(num):
-    return str(num)
+def frett(num):
+    return render_template("content.html",titill=frettir[num][0],texti=frettir[num][1],hofundur=frettir[num][2])
 @app.errorhandler(404)
 def page_not_found(e):
     return "Þessi sýða fannst ekki(404 error)"
